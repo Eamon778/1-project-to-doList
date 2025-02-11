c8286ea5ac2273da9983bc72ea4478fea0373eb9
Eamon Ali <eamonmondol525@gmail.com>
2025-02-11 21:52:35 +0600

    creating frontend;

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { IoMdAddCircle } from 'react-icons/io'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    return (
+      <div className="bg-blue-950 h-screen">
+        <div className="lg:container mx-auto">
+          <h1 className="text-5xl text-white font-extrabold">To Do</h1>
+          <div className="flex justify-between text-white pt-7">
+            <p>Lists</p>
+            <button className="flex items-center text-lg font-bold gap-1 px-3 py-2 rounded-lg hover:bg-gray-600">Add <IoMdAddCircle /></button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
 
 export default App
